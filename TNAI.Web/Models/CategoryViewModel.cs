3997fd6401f6d45a743b69b4ae98600370350163
Hubert Klim <hubert.klim3@gmail.com>
2025-01-29 23:28:47 +0100

    feat: added categories ui

---
+++ TNAI.Web/Models/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+using TNAI.Dto.Catrgories;
+using TNAI.Model.Entities;
+
+namespace TNAI.Web.Models
+{
+    public class CategoryViewModel
+    {
+        public Category category;
+        public List<ProductDto> products;
+    }
+}
+
