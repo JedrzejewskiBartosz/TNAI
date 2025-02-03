50fbf2a60b039bb8ec12c0075f2c722214aaeea8
Bartosz Jędrzejewski <118112054+JedrzejewskiBartosz@users.noreply.github.com>
2025-02-03 20:30:17 +0100

    MVC- proj
    MVC

---
+++ StoreApp.Models/Models/ApplicationUserModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Models.Models
+{
+    public class ApplicationUserModel: IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
--- TNAI.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TNAI.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
