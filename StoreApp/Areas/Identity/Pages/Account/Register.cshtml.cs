4691b157020e248e0727e23228ce372166ccf2c8
igaku02 <iga.kuzdrzal@gmail.com>
2025-02-06 13:57:31 +0100

    Poprawka - tworzenie rol

--- StoreApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ StoreApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -121,7 +121,7 @@ namespace StoreApp.Areas.Identity.Pages.Account
         public async Task OnGetAsync(string returnUrl = null)
         {
 
-            if (_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
             {
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
